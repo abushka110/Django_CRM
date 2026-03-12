72371e379949c987f062dce94588e2be89900399
Oleh Donets <olehdonets110@gmail.com>
2026-03-12 20:44:32 +0100

    website base

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for dcrm project.
+URL configuration for website project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('website.urls')),
+    path('', views.home, name = 'home'),
+
 ]
