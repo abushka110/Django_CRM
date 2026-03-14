275a721d016b94eeae74f7f3db59125c24c33cc5
Oleh Donets <olehdonets110@gmail.com>
2026-03-14 17:53:49 +0100

    registration form

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     # TODO: separate login page
     # path('login/', views.login_user, name = 'login'),
     path('logout/', views.logout_user, name = 'logout'),
+    path('register/', views.register_user, name = 'register'),
 ]
