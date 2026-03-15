23a149a81ca76d54d88f30f7894d34d52830a028
Oleh Donets <olehdonets110@gmail.com>
2026-03-15 18:08:13 +0100

    translation functionality added

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.db.models import Count
+from django.utils.translation import gettext as _
 from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
@@ -15,21 +16,17 @@ def home(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "Successfully logged in!")
+            messages.success(request, _("Successfully logged in!"))
             return redirect('home')
         else:
-            messages.success(request, "Username or Password is incorrect! Please try again.")
+            messages.error(request, _("Username or Password is incorrect! Please try again."))
             return redirect('home')
     else:
-        return render(request, 'home.html', {'records':records})
-
-# TODO: separate login page
-# def login_user(request):
-#     pass
+        return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
     logout(request)
-    messages.success(request, "You have been successfully logged out!")
+    messages.success(request, _("You have been successfully logged out!"))
     return redirect('home')
 
 def register_user(request):
@@ -37,91 +34,78 @@ def register_user(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            # Authentication and login
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, "You have successfully registered!")
+            messages.success(request, _("You have successfully registered!"))
             return redirect('home')
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form': form})
 
 def add_record(request):
     form = AddRecordForm(request.POST or None)
     if request.user.is_authenticated:
         if request.method == "POST":
             if form.is_valid():
-                add_record = form.save()
-                messages.success(request, "Record Added successfully!")
+                form.save()
+                messages.success(request, _("Record Added successfully!"))
                 return redirect('home')
-        return render(request, 'add_record.html', {'form':form})
+        return render(request, 'add_record.html', {'form': form})
     else:
-        messages.success(request, "You must be logged in!")
+        messages.error(request, _("You must be logged in!"))
         return redirect('home')
 
 def customer_record(request, pk):
     if request.user.is_authenticated:
-        # Look Up Records
         customer_record = Record.objects.get(id=pk)
-        return render(request, 'record.html', {'customer_record':customer_record})
+        return render(request, 'record.html', {'customer_record': customer_record})
     else:
-        messages.success(request, "You must be logged in!")
+        messages.error(request, _("You must be logged in!"))
         return redirect('home')
-    
+
 def delete_record(request, pk):
     if request.user.is_authenticated:
         delete_record = Record.objects.get(id=pk)
         delete_record.delete()
-        messages.success(request, "Record has been deleted!")
+        messages.success(request, _("Record has been deleted!"))
         return redirect('home')
     else:
-        messages.success(request, "You must be logged in!")
+        messages.error(request, _("You must be logged in!"))
         return redirect('home')
-    
+
 def update_record(request, pk):
     if request.user.is_authenticated:
         current_record = Record.objects.get(id=pk)
         form = AddRecordForm(request.POST or None, instance=current_record)
         if form.is_valid():
             form.save()
-            messages.success(request, "Record has been Updated successfully!")
+            messages.success(request, _("Record has been Updated successfully!"))
             return redirect('view_record', pk=current_record.id)
         return render(request, 'update_record.html', {
             'form': form,
             'current_record': current_record
         })
     else:
-        messages.success(request, "You must be logged in!")
+        messages.error(request, _("You must be logged in!"))
         return redirect('home')
-
 
 def dashboard(request):
     if request.user.is_authenticated:
-        # total customers
         total_customers = Record.objects.count()
-
-        # customers by state
         customers_by_state = (
             Record.objects.values('state')
             .annotate(total=Count('state'))
             .order_by('-total')
         )
 
-        # email domains
         emails = Record.objects.values_list('email', flat=True)
-
-        domains = []
-        for email in emails:
-            if email and '@' in email:
-                domains.append(email.split('@')[1])
+        domains = [email.split('@')[1] for email in emails if email and '@' in email]
 
         domain_counts = {}
         for d in domains:
             domain_counts[d] = domain_counts.get(d, 0) + 1
-
-        # convert to sorted list
         domain_counts = sorted(domain_counts.items(), key=lambda x: x[1], reverse=True)
 
         context = {
@@ -129,8 +113,7 @@ def dashboard(request):
             "customers_by_state": customers_by_state,
             "domain_counts": domain_counts[:5]
         }
-
         return render(request, "dashboard.html", context)
     else:
-        messages.success(request, "You must be logged in!")
+        messages.error(request, _("You must be logged in!"))
         return redirect('home')
